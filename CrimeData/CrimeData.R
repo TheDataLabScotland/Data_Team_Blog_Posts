914b3482f4f746fa06082f32f5a24dc93cfe88f2
CaterinaC <caterina.constantinescu@gmail.com>
2017-10-11 18:39:48 +0100

    Added clarifications in code and more gradual approach to plotting

--- CrimeData/CrimeData.R
+++ CrimeData/CrimeData.R
@@ -1,17 +1,18 @@
 
 library( data.table )
+library( tidyr )
 library( stringr )
 library( lubridate )
 
 library( ggplot2 )	
 library( ggmap )
 library( ggrepel )
-library( mapproj )	
-require( RColorBrewer )	
+# library( mapproj )	
+# require( RColorBrewer )	
 
 setwd("/home/u010/c_constant/Documents/BlogPosts/CrimeData")
 
-crime <- fread( "CrimeData.csv", na.strings = "" )
+crime <- fread( "CrimeData.csv", na.strings = c( "", "NA", "-", "X" ) )
 
 head( crime )
 lat_and_long <- str_split( crime$Location, ", " )
@@ -24,73 +25,89 @@ long <- as.numeric( str_replace( long, "\\)", "" ) )
 crime[ , lat := lat ]
 crime[ , long := long ]
 
-setnames( crime, str_replace( names( crime ), " ", "" ) )
+setnames( crime, str_replace_all( names( crime ), " ", "" ) )
 
 
 crime[ , DateOccurred := as.Date( DateOccurred, format = "%m/%d/%Y" ) ]
+crime[ , MonthOccurred := lubridate::month( DateOccurred, label = TRUE ) ]
 crime[ , DateReported := as.Date( DateReported, format = "%m/%d/%Y" ) ]
 
 crime[ , YearOccurred := year( DateOccurred ) ]
 crime[ , YearReported := year( DateReported ) ]
 
-table( crime$YearOccurred )
-table( crime$YearReported )
-table( crime$TimeOccurred )
+# Break the multiple MOCodes specified per cell into different cells/columns
+crime <- separate( crime, "MOCodes", into = paste( "MOCode", 1:10, sep = "_" ), sep = " " )
 
-map <- get_map( location = 'Los Angeles', zoom = 12, maptype = "roadmap" )
+
+
+map <- get_map( location = 'Los Angeles', zoom = 10, maptype = "roadmap" )
+
+
+# Version 1: simplest form
+bare_version <- ggmap( map ) +
+  geom_point( data = crime[ TimeOccurred > 2300 & VictimAge < 18 & YearOccurred > 2011, ], 
+              aes_string( x = long, y = lat ), 
+              alpha = .15, color = "black", size = 2 ) +
+  labs( x = "Longitude", y = "Latitude", title = "LA criminal activity" )
+bare_version
+
+# Version 2: creating a facet
+version_with_one_facet <- bare_version + facet_wrap( ~ YearOccurred, ncol = 3 )
+version_with_one_facet
+
+# Version 3: using multiple facets
+version_with_two_facets <- bare_version + facet_grid( YearOccurred ~ MonthOccurred )
+version_with_two_facets
+
+# Remove x and y axis tick marks & labels due to clutter
+version_with_two_facets_no_labels <- version_with_two_facets +
+  theme( axis.text.x = element_blank(),
+         axis.ticks.x = element_blank(),
+         axis.text.y = element_blank(),
+         axis.ticks.y = element_blank() )
+version_with_two_facets_no_labels
+
+
+# Isolating a specific type of crime, to spot any trends: For this, I have to check all columns (MOCode_1 to MOCode_10) for the following values:
+
+Internet_crimes <- c( "1904", "1905", "1908", "1909", "1911", "1914", "1915", "1916" )
+narcostics_prostitution <- c( "0907", "0908" )
+# crime[ MOCodes %in% ,  ]
+
+Internet_crime_data <- crime[ MOCode_1 %in% Internet_crimes | MOCode_2 %in% Internet_crimes | MOCode_3 %in% Internet_crimes |
+                                MOCode_4 %in% Internet_crimes | MOCode_5 %in% Internet_crimes | MOCode_6 %in% Internet_crimes |
+                                MOCode_7 %in% Internet_crimes | MOCode_8 %in% Internet_crimes | MOCode_9 %in% Internet_crimes |
+                                MOCode_10 %in% Internet_crimes, ]
+
+# Swap data used previously with this plot:
+replot <- get('%+%', 'package:ggplot2')
+
+
+replot(version_with_two_facets_no_labels, Internet_crime_data)
+attach(Internet_crime_data)
+version_with_two_facets_no_labels %+% Internet_crime_data
+detach(Internet_crime_data)  
+
+
+# Label some of the points
+pdf( 'CrimeDataBogusPlot.pdf', width = 10, height = 8 )
 ggmap( map ) +
   geom_point( data = crime[ TimeOccurred > 2300 & VictimAge < 18 & YearOccurred > 2011, ], 
-              aes( x = long, y = lat, color = TimeOccurred ), 
-              alpha = .35 ) + 
-  geom_label_repel(data = crime[ TimeOccurred > 2300 & VictimAge < 18 & YearOccurred > 2011, ], 
-                   aes(x = long, y = lat, label = VictimDescent ), 
-                   fill = "white", box.padding = unit(.4, "lines"),
-                   label.padding = unit(.15, "lines"),
-                   segment.color = "red", segment.size = 1) +
-  facet_grid( VictimSex ~ YearOccurred )
-
-# myplot <- with(crime, plot(TimeOccurred, lat))
-# pdf('./Desktop/CrimeDataBogusPlot.pdf', width = 10, height = 10)
-# myplot
-# dev.off()
-
-
-# source( "MyCloudMadeAPI.R")
-# ?get_cloudmademap
-# qmap( "baylor university", zoom = 14, maptype = 53428, api_key = cloudmade_api_key,
-#       source = "cloudmade" )
-
-
-
-# Can I anonymize this data? ----------------------------------------------
-
-# This is buggy right now:
-
-library(fakeR)
-
-x <- crime[ 1:200, ]
-# fake_x <- simulate_dataset( data.frame( x$DRNumber ), use.levels = FALSE )
-lapply(x, is.factor)
-
-fake_cols <- list()
-for ( column in 1 : ncol( x ) ) {
-   fake_cols[[column]] <- simulate_dataset( as.vector( x[ , column, with = FALSE ] ), use.levels = FALSE, stealth.level = 3, level3.noise = TRUE )
-}
-
-# Strange. fakeR seems to not work on x as a whole data.table or data.frame. The only way I can get it to work is to cycle thru the columns individually - which then, the simulate_dataset() function outputs as 
-
-# lapply(fake_cols, dim)
-# fake_x <- rbindlist( fake_cols )
-# y <- simulate_dataset( x )
-
-
-xyz <- data.table( sapply( fake_cols, "[[", 1 ) )
-setnames( xyz, names( x ) )
-
-
-table( x$VictimSex )
-table( xyz$VictimSex ) # So this thing isn't JUST shuffling the data.
-
+              aes( x = long, y = lat, color = StatusDescription ), 
+              alpha = .35 ) +
+  facet_grid( YearOccurred ~ MonthOccurred ) +
+  theme( axis.title.x = element_blank(),
+         axis.text.x = element_blank(),
+         axis.ticks.x = element_blank(),
+         axis.title.y = element_blank(),
+         axis.text.y = element_blank(),
+         axis.ticks.y = element_blank() ) +
+  geom_label_repel( data = crime[ TimeOccurred > 2300 & VictimAge < 18 & YearOccurred > 2011, ],
+                    aes( x = long, y = lat, label = VictimDescent ),
+                    fill = "white", box.padding = unit( 0.4, "lines" ),
+                    label.padding = unit( 0.15, "lines" ),
+                    segment.color = "red", segment.size = 1 )
+dev.off()
 
 
 
