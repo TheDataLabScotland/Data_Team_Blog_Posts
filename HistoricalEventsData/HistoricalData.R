c9763c217afaf1f16e56b506dc070c3eccc55fd8
CaterinaC <caterina.constantinescu@gmail.com>
2017-10-19 16:02:15 +0100

    Updated HistoricalData.R script and done more data manipulation

--- HistoricalEventsData/HistoricalData.R
+++ HistoricalEventsData/HistoricalData.R
@@ -1,11 +1,15 @@
 
 library( data.table )
 library( ggplot2 )
+library( stringi )
 library( stringr )
+library( plyr )
 
 options( scipen = 999 )
 
-setwd( "/home/u010/c_constant/Documents/BlogPosts/HistoricalEventsData" )
+setwd( "/scratch/u010/c_constant/Documents/BlogPosts/HistoricalEventsData" )
+# load("HistoricalDataRObjects.RData")
+
 
 BBC_events <- fread( "BBC_Summary_of_World_Broadcasts_1979_2015_.csv", na.strings = "" )
 BBC_meta <- fread( "BBC_Summary_of_World_Broadcasts_1979_2015_MetaData.csv", na.strings = "" )
@@ -47,11 +51,71 @@ length( unique( BBC_events$target ) )
 
 # These source and target codes dont mean much in themselves. So I'm going to match them up to the explanations from the codebook.
 
-source_target_codes <- fread( "SourceOrTargetCodes_ClineBBCData.csv" )
+source_target_dict <- fread( "SourceOrTargetCodes_ClineBBCData.csv" )
 
+# Remove punctuation from these character vars:
 BBC_events[ , target := str_trim( str_extract( target, "[[:alpha:]]+" ), side = "both" ) ]
 BBC_events[ , source := str_trim( str_extract( source, "[[:alpha:]]+" ), side = "both" ) ]
 
-# Gotta split strings into groups of 3... This is not working yet:
-str_replace( head( BBC_events$target ), "(.{3})", "\\1" )
-# gsub("(.{3})", "\\1", head( BBC_events$target ) )
+table( nchar( BBC_events$target ) )
+table( nchar( BBC_events$source ) )
+
+
+# Gotta split strings into groups of 3... 
+targets_list <- stri_extract_all_regex( BBC_events$target, '.{1,3}' )
+targets_data_matrix <- plyr::ldply( targets_list, rbind )
+setnames( targets_data_matrix, paste( "target", 1:9, sep = "_" ) )
+
+sources_list <- stri_extract_all_regex( BBC_events$source, '.{1,3}' )
+sources_data_matrix <- plyr::ldply( sources_list, rbind )
+setnames( sources_data_matrix, paste( "source", 1:9, sep = "_" ) )
+
+# Get a measure of how many sources and or targets an entry has. Presumably, the bigger the event, the more sources it's picked up by, and the more targets it involves.
+BBC_events[ , total_sources := unlist( lapply( sources_list, length ) ) ]
+BBC_events[ , total_targets := unlist( lapply( targets_list, length ) ) ]
+
+# Replace each code by its actual meaning to help with deciphering dataset:
+targets_data_matrix <- data.table( mapvalues( as.matrix( targets_data_matrix ), 
+                                              from = source_target_dict$Code, 
+                                              to = source_target_dict$`Source/Target` ) )
+sources_data_matrix <- data.table( mapvalues( as.matrix( sources_data_matrix ), 
+                                              from = source_target_dict$Code, 
+                                              to = source_target_dict$`Source/Target` ) )
+
+full_BBC_events <- data.table( BBC_events, sources_data_matrix, targets_data_matrix )
+
+setnames( BBC_meta, "pubdate", "story_date" )
+BBC_meta[ , story_date := as.Date( story_date, format = "%m/%d/%Y" ) ]
+
+full_BBC_events_with_meta <- join( full_BBC_events, BBC_meta, by = c( "aid", "story_date" ) )
+
+tail( full_BBC_events_with_meta )
+
+# Remove unnecessary columns for simplicity:
+full_BBC_events_with_meta$original_source <- NULL
+full_BBC_events_with_meta$process <- NULL
+
+# Trying to understand the structure of this dataset:
+table( table( full_BBC_events_with_meta$eid ) ) # Seems like events are unique
+table( table( full_BBC_events_with_meta$aid ) ) # Seems like articles repeat themselves, confusingly
+table( table( full_BBC_events_with_meta$code ) ) # event's code for: Conflict and Mediation Event Observation (CAMEO code)
+table( table( full_BBC_events_with_meta$root_code ) ) # Super-ordinate CAMEO code, with following dictionary:
+
+CAMEO_root_code <- 1 : 20
+CAMEO_label <- c( "Make public statement", "Appeal", "Express intent to cooperate",
+                  "Consult", "Engage in diplomatic cooperation", 
+                  "Engage in diplomatic cooperation", "Provide aid", "Yield", 
+                  "Investigate", "Demand", "Disapprove", "Reject", "Threaten", 
+                  "Protest", "Exhibit force posture", "Reduce relations", "Coerce",
+                  "Assault", "Fight", "Use unconventional mass violence")
+
+full_BBC_events_with_meta[ , root_code := mapvalues( root_code, CAMEO_root_code, CAMEO_label ) ]
+
+
+
+
+
+save.image("HistoricalDataRObjects.RData")
+
+
+
