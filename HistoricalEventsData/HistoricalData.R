bf12a3c2a63289b0933b292b372ee86a86fe4c7d
CaterinaC <caterina.constantinescu@gmail.com>
2017-11-02 16:04:31 +0000

    Significantly improved historical events code

--- HistoricalEventsData/HistoricalData.R
+++ HistoricalEventsData/HistoricalData.R
@@ -10,11 +10,16 @@ library( stringi )
 library( stringr )
 library( plyr )
 library( rworldmap )
+library( devtools )
+# install_github("dgrtwo/gganimate")
+library( gganimate )
 
 options( scipen = 999 )
 
-setwd( "C:/Users/cconsta2/WinToUbuntuAndBackAgain/HistoricalEventsData" )
-# load("HistoricalDataRObjects.RData")
+setwd( "your/path/goes/here" )
+load( "HistoricalDataRObjects.RData" )
+
+
 
 
 
@@ -142,34 +147,204 @@ table( full_BBC_events_with_meta$statename )
 
 # Visualizing the data ----------------------------------------------------
 
+# Many dimensions with many sub-categories each... hard to pick what to show.
 
-average_goldstein <- data.table( aggregate( cbind( goldstein, total_sources ) ~ 
-                                              year + continent,
-                                            FUN = mean,
-                                            data = full_BBC_events_with_meta ) )
+# Averaging goldstein ratings over country and by year:
+goldstein_country_in_continent_by_year <- data.table( aggregate( cbind( goldstein, total_sources ) ~ 
+                                                                   year + countryname + continent,
+                                                                 FUN = mean,
+                                                                 data = full_BBC_events_with_meta ) )
+# goldstein_country_in_continent_by_year <- goldstein_country_in_continent_by_year[ order( continent, -goldstein ), ]
+# goldstein_country_in_continent_by_year[ , ]
 
+# And now collapsing across years:
+goldstein_country_in_continent <- data.table( aggregate( cbind( goldstein, total_sources ) ~ 
+                                                                   countryname + continent,
+                                                                 FUN = mean,
+                                                                 data = full_BBC_events_with_meta ) )
+goldstein_country_in_continent <- goldstein_country_in_continent[ order( continent, goldstein ), ]
+corect_order <- unique( goldstein_country_in_continent$countryname )
 
-ggplot( average_goldstein, 
-        aes( y = goldstein, x = continent ) ) +
-  geom_boxplot( position = "dodge", varwidth = FALSE ) +
-  facet_wrap( ~ year, drop = TRUE, ncol = 5 ) +
-  geom_point( aes( color = total_sources ), alpha = 0.1, size = 0.5 ) +
-  theme( axis.text.x = element_text( angle = 90, hjust = 1, size = 0.7 ) )
+setnames( goldstein_country_in_continent, 
+          c( "goldstein", "total_sources" ), 
+          c( "country_goldstein", "country_total_sources" ) )
 
+two_level_aggregates <- join( goldstein_country_in_continent_by_year, 
+                              goldstein_country_in_continent )
+two_level_aggregates[ , countryname := ordered( countryname, levels = corect_order ) ] 
 
 # First 5 years seem to have next to no data. 
 # Since they are not particularly interesting, they shall be removed.
 
-# pdf("C:/Users/cconsta2/Desktop/Test.pdf", width = 15, height = 30 )
-ggplot( average_goldstein[ year > 1977, ], 
-        aes( y = goldstein, x = continent ) ) +
-  # geom_boxplot( position = "dodge", varwidth = FALSE ) +
-  facet_wrap( ~ year, drop = TRUE, ncol = 5 ) +
-  geom_bar( stat = "identity" ) +
-  theme( axis.text.x = element_text( angle = 90, hjust = 1, size = 12 ) )
+# Check spread for each country, in this pseudo-caterpillar plot which includes information on all the continents plotted together:
+
+png( "CaterpillarGeneral.png", width = 1000, height = 2400 )
+ggplot( two_level_aggregates[ year > 1977, ], 
+        aes( x = goldstein, y = countryname, 
+             group = countryname, color = continent ) ) +
+  geom_vline( xintercept = 0, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_vline( xintercept = -5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_vline( xintercept = 5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_line( stat = "identity" ) +
+  geom_point( aes( x = country_goldstein, y = countryname, color = continent ) ) +
+  # facet_wrap( ~ continent, ncol = 6 ) + 
+  xlim( -10, 10 ) +
+  theme( axis.text.x = element_text( angle = 90, hjust = 1, size = 12 ),
+         text = element_text( size = 22 ) ) +
+  guides( color = guide_legend( ncol = 1 ) ) +
+  labs( color = "Area", x = "Average Goldstein rating", y = "Country" )
+dev.off()
+
+# Here we split the graph above by continent - and show each one separately within its own panel:
+png( "CaterpillarPlotByContinent.png", width = 2800, height = 2400 )
+ggplot( two_level_aggregates[ year > 1977, ], 
+        aes( x = goldstein, y = countryname, 
+             group = countryname, color = continent ) ) +
+  geom_vline( xintercept = 0, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_vline( xintercept = -5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_vline( xintercept = 5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_line( stat = "identity" ) +
+  geom_point( aes( x = country_goldstein, y = countryname, color = continent ) ) +
+  facet_wrap( ~ continent, ncol = 4, scales = "free" ) + 
+  xlim( -10, 10 ) +
+  theme( axis.text.x = element_text( angle = 90, hjust = 1, size = 12 ),
+         text = element_text( size = 22 ) ) +
+  guides( color = FALSE ) +
+  labs( color = "Area", x = "Average Goldstein rating", y = "Country" )
+dev.off()
+
+
+
+
+# Using violin plots to show whether some countries spend more time at the extremes of the Goldstein scale.
+# Let's pick a couple of continents / areas for this:
+png( "ViolinCentralEurope.png", width = 2600, height = 3000 )
+ggplot( two_level_aggregates[ year > 1977 & continent == "Central Europe", ], 
+        aes( y = goldstein, x = countryname, 
+             group = countryname, fill = countryname ) ) +
+  geom_hline( yintercept = 0, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_hline( yintercept = -5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_hline( yintercept = 5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_violin( position = "dodge", 
+               draw_quantiles = c( 0.25, 0.50, 0.75), 
+               trim = TRUE ) +
+  geom_text( aes( x = countryname, y = 8.5, label = countryname ), angle = 90, size = 23 ) +
+  ylim( -10, 10 ) +
+  guides( fill = FALSE ) +
+  theme( axis.text.y = element_text( size = 60 ),
+         axis.title.y = element_text( size = 70 ),
+	 axis.title.x = element_blank(),
+	 axis.text.x = element_blank(),
+	 axis.ticks.x = element_blank()	) +
+  labs( y = "Average Goldstein rating" )
+dev.off()
+
+
+png( "ViolinWesternEurope.png", width = 3000, height = 3000 )
+ggplot( two_level_aggregates[ year > 1977 & continent == "Western Europe", ], 
+        aes( y = goldstein, x = countryname, 
+             group = countryname, fill = countryname ) ) +
+  geom_hline( yintercept = 0, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_hline( yintercept = -5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_hline( yintercept = 5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_violin( position = "dodge", 
+               draw_quantiles = c( 0.25, 0.50, 0.75), 
+               trim = TRUE ) +
+  geom_text( aes( x = countryname, y = 8.5, label = countryname ), angle = 90, size = 23 ) +
+  ylim( -10, 10 ) +
+  guides( fill = FALSE ) +
+  theme( axis.text.y = element_text( size = 60 ),
+	 axis.title.y = element_text( size = 70 ),
+	 axis.title.x = element_blank(),
+	 axis.text.x = element_blank(),
+	 axis.ticks.x = element_blank()	) +
+  labs( y = "Average Goldstein rating" )
+dev.off()
+
+
+# Ridiculously overplotted, and in such a way that merely setting alpha or changing symbols will not help. Also cannot use facetting really, since we already *are* within a facet (i.e., Western Europe):
+png( "TimeTrendPerCountry_Overcrowded.png", width = 1800, height = 1200 )
+ggplot( two_level_aggregates[ year > 1977 & continent == "Western Europe", ], 
+        aes( y = goldstein, x = year, 
+             group = countryname, color = countryname ) ) +
+  geom_point() + geom_line( stat = "identity" ) +
+  guides( color = guide_legend( ncol = 1 ) ) +
+  ylim( -10, 10 ) + 
+  theme( text = element_text( size = 35 ) ) +
+  labs( color = "Country", y = "Average Goldstein rating", group = "Country" )
+dev.off()
+
+
+
+# So what can we do to streamline all this information and present it gradually, but without creating extremely coarse-grained aggregates (e.g., continent-wide Goldstein averages for each year)? 
+# A rudimentary method we can try is to just select a few countries we are interested in
+png( "TimeTrendForHandfulOfCountries.png", width = 1800, height = 1200 )
+ggplot( two_level_aggregates[ year > 1977 & 
+                                countryname %in% c( "France", "Italy",
+                                                    "Finland", "Spain" ), ], 
+        aes( y = goldstein, x = year, 
+             group = countryname, 
+             color = countryname ) ) +
+  geom_point() + geom_line( stat = "identity", size = 4 ) +
+  theme( text = element_text( size = 35 ) ) +
+  labs( color = "Country", y = "Average Goldstein rating", group = "Country" ) +
+  ylim( -10, 10 )  
+dev.off()
+
+
+
+# Ok. But how can we insert both 'event importance' information, AND show changes in Goldstein ratings over time?
+# png( "test1.png", width = 1800, height = 1200 )
+ggplot( goldstein_country_in_continent[ continent == "Western Europe", ], 
+        aes( x = country_goldstein, y = countryname, 
+             group = countryname, 
+             color = countryname ) ) +
+  geom_point() + guides( color = FALSE ) +
+  xlim( -10, 10 ) 
 # dev.off()
 
+
+
+# png( "test1.png", width = 1800, height = 1200 )
+p <- ggplot( goldstein_country_in_continent_by_year[ continent == "Central Europe", ], 
+             aes( x = goldstein, y = countryname, 
+                  group = countryname, 
+                  color = countryname,
+                  size = total_sources,
+                  frame = year,
+                  cumulative = FALSE ) ) +
+  geom_vline( xintercept = 0, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_vline( xintercept = -5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_vline( xintercept = 5, color = "black", lty = "dashed", size = 0.3 ) +
+  geom_point( alpha = 0.5 ) + guides( color = FALSE ) +
+  xlim( -10, 10 ) +
+  theme( text = element_text( size = 16 ) ) +
+  labs( size = "Event importance", x = "Average Goldstein rating", y = "Country" )
+# dev.off()
+
+gganimate( p, filename = "~/Desktop/CentralEurope.gif", ani.width = 800, ani.height = 500 ) # , interval = 1.5
+
+
+# > unique(goldstein_country_in_continent_by_year$continent)
+# [1] "Antarctic"                 "Arabian Peninsula"         "Australia and New Zealand" "Canada"                   
+# [5] "Caribbean"                 "Central Africa"            "Central Asia"              "Central Europe"           
+# [9] "Eastern Africa"            "Eastern Europe"            "Mashriq"                   "Meso-America"             
+# [13] "North Africa"              "NW Pacific and East Asia"  "Polar"                     "South America"            
+# [17] "South Asia"                "South Pacific"             "Southeast Asia"            "Southern Africa"          
+# [21] "US"                        "Western Africa"            "Western Europe"            "Western Indian Ocean" 
+
+
+# Now to swap this subset for another:
+p2 <- p %+% goldstein_country_in_continent_by_year[ continent == "Western Africa", ]
+gganimate( p2, filename = "~/Desktop/BadPlace.gif", ani.width = 800, ani.height = 500 ) # , interval = 1.5
+
+
+
+
+
+
 # save.image("HistoricalDataRObjects.RData")
+
 
 
 
